302d528c1f161c24a8d0635a43d31f754dfbd221
Mi Xiaobing <xiaomi@xingke888.com>
2023-07-25 06:39:54 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,7 +118,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          香港高才联合会
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
