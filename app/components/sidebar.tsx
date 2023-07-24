e8473d0efd7677930cc35b64797443f2803b8cf2
Mi Xiaobing <xiaomi@xingke888.com>
2023-07-25 07:15:44 +0800

    1

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
           香港高才联合会
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          AI办公平台.
+          AI办公赋能平台.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
